e20886325ac8d5cfa579cb99832f0909e8c82758
nakaotatsuya <earthdefense10@gmail.com>
2020-08-10 15:55:49 +0900

    Initial Commit

---
+++ Sample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample2
+//
+//  Created by 中尾　達也 on 2020/08/10.
+//  Copyright © 2020 Sample Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
